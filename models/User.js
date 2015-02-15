6e8c468d1db91848230dca8ce7c4affbbbe29e26
vagrant <vagrant@precise32.(none)>
2015-02-15 09:12:57 +0000

    Create the Persona Schema

---
+++ models/User.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
+
+//Personas: {Nombre, Balance, NBajada, NPagado}
+var personaSchema = new mongoose.Schema({
+	email: {type:String, unique: true},
+	password: String,
+	nombre: String,
+	balance: Number,
+	nBajadas: Number,
+	NPagado: Number
+});
+
+personaSchema.pre('save', function(next){
+	var persona = this;
+	if(!persona.isModified('password')) return next();
+	bcrypt.genSalt(10, function(err, salt){
+		if (err) return next(err);
+		bcrypt.hash(persona.password, salt, function(err, hash){
+			if(err) return next(err);
+			persona.password = hash;
+			next();
+		});
+	});
+});
+
+personaSchema.methods.comparePassword = function(candidatePassword, cb){
+	bcrypt.compare(candidatePassword, this.password, function(err, isMatch){
+		if(err) return cb(err);
+		cb(null, isMatch);
+	});
+};
