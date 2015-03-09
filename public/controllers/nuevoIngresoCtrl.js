95197a88a52a1583142988e9441cf978b0389668
Iker Obregon <yamaksy@gmail.com>
2015-03-09 11:21:50 -0400

    Redirect to / after a new 'Ingreso'

--- public/controllers/nuevoIngresoCtrl.js
+++ public/controllers/nuevoIngresoCtrl.js
@@ -1,9 +1,10 @@
 angular.module('Coffee-time')
-  .controller('NuevoIngresoCtrl', ['$scope', '$http', function($scope, $http) {
+  .controller('NuevoIngresoCtrl', ['$scope', '$http','$location', function($scope, $http, $location) {
 	  $scope.nuevoingreso = function(){
 	  	$http.post('/api/ingreso', $scope.Ingreso)
 	  	.success(function(data, status, headers, config){
 	  		$scope.Ingreso = {};
+			$location.path('/');
 	  	})
 	  	.error(function(data, status, header, config){
 	  		alert(data);
