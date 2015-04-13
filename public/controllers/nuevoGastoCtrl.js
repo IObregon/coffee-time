27a30e2df393e7d2a8483f564bd722ed5c7d0e21
Iker Obregon <yamaksy@gmail.com>
2015-04-13 10:59:18 -0400

    Solve a bug that deletes the last added 'Gasto' instead of the last of that user

--- public/controllers/nuevoGastoCtrl.js
+++ public/controllers/nuevoGastoCtrl.js
@@ -39,7 +39,7 @@ angular.module('Coffee-time')
 	}
 
 	$scope.eliminarGasto = function(){
-		$http.delete('/api/gasto/' + gasto._id)
+		$http.delete('/api/gasto/' + $scope.gasto._id)
 			.success(function(data){
 				$location.path('/');
 			})
